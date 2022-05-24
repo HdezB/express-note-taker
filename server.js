5c77460f6ae4add655ead98f350bee78cf5301a6
DukeHdez <briayan27@outlook.com>
2022-05-24 12:58:13 -0400

    Routes for the index.html created

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./Develop/routes/apiRoutes');
+const apiRoutes = ('./Develop/routes/apiRoutes');
 const htmlRoutes = require('./Develop/routes/htmlRoutes');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', apiRoutes);
+app.use('/notes', apiRoutes)
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
